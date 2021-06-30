4ec645e7f198c94a5a4809bcb84995ba13b9b1f2
strophy <badfunkstripe@gmail.com>
2021-06-30 14:38:40 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gh-action-cache-buildkit-state
---
+++ main.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+
+const { exec } = require('child_process');
+const { promisify } = require('util');
+
+const execAsync = promisify(exec);
+
+const builder = core.getInput('builder');
+const path = core.getInput('cache-path');
+
+async function run() {
+  await execAsync('docker buildx stop');
+
+  await execAsync(`docker run --rm \
+    --volumes-from buildx_buildkit_${ builder }0 \
+    -v ${ path }:/cache \
+    alpine /bin/sh -c "cd / && tar xf /cache/buildkit-state.tar && echo Cache restored successfully"`);
+}
+
+async function post() {
+  await execAsync(`docker buildx prune --force --keep-storage ${ core.getInput('cache-max-size')}`)
+
+  await execAsync(`docker run --rm \
+    --volumes-from buildx_buildkit_${ builder }0 \
+    -v ${ path }:/cache \
+    alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar /var/lib/buildkit && echo Cache archived successfully"`);
+}
+
+// check if isPost is present in the action state
+const isPost = !!core.getState('isPost')
+
+if (!isPost) {
+    run()
+} else {
+    post()
+}
