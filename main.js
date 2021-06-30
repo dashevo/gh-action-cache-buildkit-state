3b20aa489b1bf522174649f5118d92aa123666eb
strophy <badfunkstripe@gmail.com>
2021-06-30 14:54:45 +0200

    fix: do not assume builder prefix/suffix

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ async function run() {
   await execAsync('docker buildx stop');
 
   await execAsync(`docker run --rm \
-    --volumes-from buildx_buildkit_${ builder }0 \
+    --volumes-from ${ builder } \
     -v ${ path }:/cache \
     alpine /bin/sh -c "cd / && tar xf /cache/buildkit-state.tar && echo Cache restored successfully"`);
 }
@@ -21,7 +21,7 @@ async function post() {
   await execAsync(`docker buildx prune --force --keep-storage ${ core.getInput('cache-max-size')}`)
 
   await execAsync(`docker run --rm \
-    --volumes-from buildx_buildkit_${ builder }0 \
+    --volumes-from ${ builder } \
     -v ${ path }:/cache \
     alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar /var/lib/buildkit && echo Cache archived successfully"`);
 }
