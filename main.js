14a9d7124d323e701d3b2c9c3c557cec8a18a19e
strophy <badfunkstripe@gmail.com>
2021-06-30 16:12:02 +0200

    fix: cannot interpret zx with node12

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 const core = require('@actions/core');
 
+const { exec } = require('child_process');
+const { promisify } = require('util');
+
+const execAsync = promisify(exec);
+
 const builder = core.getInput('builder');
 const path = core.getInput('cache-path');
 
 async function run() {
-  await $`docker buildx stop`;
+  await execAsync('docker buildx stop');
 
-  await $`docker run --rm \
-    --volumes-from ${builder} \
-    -v ${path}:/cache \
-    alpine /bin/sh -c "cd / && tar xf /cache/buildkit-state.tar && echo Cache restored successfully`;
+  await execAsync(`docker run --rm \
+    --volumes-from ${ builder } \
+    -v ${ path }:/cache \
+    alpine /bin/sh -c "cd / && tar xf /cache/buildkit-state.tar && echo Cache restored successfully"`);
 }
 
 async function post() {
-  await `docker buildx prune --force --keep-storage ${core.getInput('cache-max-size')}`;
+  await execAsync(`docker buildx prune --force --keep-storage ${ core.getInput('cache-max-size')}`)
 
-  await `docker run --rm \
-    --volumes-from ${builder} \
-    -v ${path}:/cache \
-    alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar /var/lib/buildkit && echo Cache archived successfully"`;
+  await execAsync(`docker run --rm \
+    --volumes-from ${ builder } \
+    -v ${ path }:/cache \
+    alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar /var/lib/buildkit && echo Cache archived successfully"`);
 }
 
 // check if isPost is present in the action state
-const isPost = !!core.getState('isPost');
+const isPost = !!core.getState('isPost')
 
 if (!isPost) {
-  run();
+    run()
 } else {
-  post();
+    post()
 }
