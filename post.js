eae3404709737df1fbbb69d275dbe0d95189f9fd
Nat Budin <natbudin@gmail.com>
2021-07-02 11:39:15 -0700

    fix: Avoid relying on state by splitting into 2 files

--- post.js
+++ post.js
@@ -8,19 +8,19 @@ const execAsync = promisify(exec);
 const builder = core.getInput('builder');
 const path = core.getInput('cache-path');
 
-async function run() {
+async function post() {
   try {
-    await execAsync('docker buildx stop');
+    await execAsync(`docker buildx prune --force --keep-storage ${ core.getInput('cache-max-size')}`)
 
     await execAsync(`docker run --rm \
       --volumes-from ${ builder } \
       -v ${ path }:/cache \
-      alpine /bin/sh -c "cd / && tar xf /cache/buildkit-state.tar"`);
+      alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar /var/lib/buildkit"`);
 
-    core.info('Cache restored successfully');
+    core.info('Cache archived successfully');
   } catch (error) {
-    core.error(`Cache restore failed: ${ error }`)
+    core.error(`Cache archive failed: ${ error }`);
   }
 }
 
-run()
+post()
