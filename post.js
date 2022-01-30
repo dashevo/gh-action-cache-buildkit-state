cc9151f3326a9675b6a56eb647c9961f18486c99
Adam Gardner <adam.gardner@magicmemories.com>
2022-01-30 12:05:35 -1000

    Fix logging

--- post.js
+++ post.js
@@ -10,36 +10,36 @@ const path = core.getInput('cache-path');
 
 async function post() {
   try {
-    console.log("step1")
+    core.info("step1")
     await execAsync(`docker system df -v`, {}, function(error,stdout,stderr) {
       if (error) {
-        console.error(`exec error: ${error}`)
+        core.error(`exec error: ${error}`)
         return
       }
-      console.log(`stdout: ${stdout}`)
-      console.error(`stderr: ${stderr}`)
+      core.info(`stdout: ${stdout}`)
+      core.error(`stderr: ${stderr}`)
     })
-    console.log("step2")
+    core.info("step2")
     await execAsync(`docker buildx du -v`, {}, function(error,stdout,stderr) {
       if (error) {
-        console.error(`exec error: ${error}`)
+        core.error(`exec error: ${error}`)
         return
       }
-      console.log(`stdout: ${stdout}`)
-      console.error(`stderr: ${stderr}`)
+      core.info(`stdout: ${stdout}`)
+      core.error(`stderr: ${stderr}`)
     })
-    console.log("step3")
+    core.info("step3")
     await execAsync(`docker exec ${ builder } buildctl du -v`, {}, function(error,stdout,stderr) {
       if (error) {
-        console.error(`exec error: ${error}`)
+        core.error(`exec error: ${error}`)
         return
       }
-      console.log(`stdout: ${stdout}`)
-      console.error(`stderr: ${stderr}`)
+      core.info(`stdout: ${stdout}`)
+      core.error(`stderr: ${stderr}`)
     })
-    console.log("step4")
+    core.info("step4")
     await execAsync(`docker buildx prune --force --keep-storage ${ core.getInput('cache-max-size')}`)
-    console.log("step5")
+    core.info("step5")
     await execAsync(`docker run --rm \
       --volumes-from ${ builder } \
       -v ${ path }:/cache \
