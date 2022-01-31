69f41bf60257eeb593d287293f9cae4be138f251
Adam Gardner <adam.gardner@magicmemories.com>
2022-01-30 16:27:01 -1000

    Fix incorrect variable name

--- post.js
+++ post.js
@@ -13,44 +13,44 @@ async function post() {
     core.info("Before pruning:");
     var {stdout, stderr} = await execAsync(`docker exec ${builder} buildctl du`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker buildx prune --force --filter type=regular`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker buildx prune --force --filter type=source.local`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker buildx prune --force --filter type=frontend`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker buildx prune --force --filter type=internal`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker buildx prune --force --filter type=exec.cachemount --keep-storage ${core.getInput('cache-max-size')}`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     core.info("After pruning:");
 
     var {stdout, stderr} = await execAsync(`docker exec ${builder} buildctl du`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker exec ${builder} buildctl du -v`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     var {stdout, stderr} = await execAsync(`docker run --rm \
       --volumes-from ${builder} \
       -v ${path}:/cache \
       alpine /bin/sh -c "cd / && tar cf /cache/buildkit-state.tar var/lib/buildkit"`);
     core.info(`stdout:\n${stdout}\n`);
-    if (error) { core.error(`stderr:\n${stderr}\n`); }
+    if (stderr) { core.error(`stderr:\n${stderr}\n`); }
 
     core.info("Cache archived successfully");
   } catch (error) {
